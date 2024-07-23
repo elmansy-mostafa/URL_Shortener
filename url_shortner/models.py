c0a638b51bc9d8918db6bba9e215a19d4ce6b58c
Mostafa Elmansy <mostafaelmansy981125@gmail.com>
2024-07-23 23:57:35 +0300

    create models
    create models.py in the project

---
+++ url_shortner/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Table, column, String, MetaData
+
+metadata = MetaData()
+urls = Table(
+    "urls",
+    metadata,
+    column("key", String, primary_key=True, index=True)
+    column("long_url", String, unique=True)
+)
